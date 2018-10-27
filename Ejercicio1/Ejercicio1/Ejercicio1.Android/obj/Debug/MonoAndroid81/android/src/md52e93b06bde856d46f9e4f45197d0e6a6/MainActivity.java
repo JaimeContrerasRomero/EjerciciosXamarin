ffb855824967169bc3b4820795b4f512f72c7c71
JaimeContrerasRomero <jaime.contreras.romero@gmail.com>
2018-10-27 11:58:11 -0500

    EJERCICIOS 1 Y 2
    SE SUBEN LOS EJERCICIOS 1 Y 2

---
+++ Ejercicio1/Ejercicio1/Ejercicio1.Android/obj/Debug/MonoAndroid81/android/src/md52e93b06bde856d46f9e4f45197d0e6a6/MainActivity.java
@@ -0,0 +1,47 @@
+package md52e93b06bde856d46f9e4f45197d0e6a6;
+
+
+public class MainActivity
+	extends md51558244f76c53b6aeda52c8a337f2c37.FormsAppCompatActivity
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_onCreate:(Landroid/os/Bundle;)V:GetOnCreate_Landroid_os_Bundle_Handler\n" +
+			"";
+		mono.android.Runtime.register ("Ejercicio1.Droid.MainActivity, Ejercicio1.Android", MainActivity.class, __md_methods);
+	}
+
+
+	public MainActivity ()
+	{
+		super ();
+		if (getClass () == MainActivity.class)
+			mono.android.TypeManager.Activate ("Ejercicio1.Droid.MainActivity, Ejercicio1.Android", "", this, new java.lang.Object[] {  });
+	}
+
+
+	public void onCreate (android.os.Bundle p0)
+	{
+		n_onCreate (p0);
+	}
+
+	private native void n_onCreate (android.os.Bundle p0);
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
